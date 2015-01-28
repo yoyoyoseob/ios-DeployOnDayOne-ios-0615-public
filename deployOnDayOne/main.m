faa1f0752e02732543aba62924cbf8ce2508bdbb
zdrossman <zachary.drossman@gmail.com>
2015-01-28 11:28:06 -0500

    Added base project

---
+++ deployOnDayOne/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  deployOnDayOne
+//
+//  Created by Zachary Drossman on 1/28/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
